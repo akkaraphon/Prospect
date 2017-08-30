8abb301bafb44398135e7a12051f3cdffca9cd79
akkaraphon.pim@gmail.com <kaitoukid>
2017-08-30 17:49:34 +0700

    30/08/2560 17.48

--- app/src/main/java/com/example/pmakkaraphon/prospect/ProfileFragment.java
+++ app/src/main/java/com/example/pmakkaraphon/prospect/ProfileFragment.java
@@ -7,9 +7,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -25,7 +24,7 @@ public class ProfileFragment extends Fragment {
     TextView mDatesave;
     TextView mDatecome;
     Spinner mPrefix;
-    RadioButton mChkAdd;
+    CheckBox mChkAdd;
     LinearLayout mLayoutCompeny;
 
     View v;
@@ -41,7 +40,7 @@ public class ProfileFragment extends Fragment {
         mDatesave = (TextView) v.findViewById(R.id.txtDatesave);
         mDatecome = (TextView) v.findViewById(R.id.txtDatecome);
         mPrefix = (Spinner) v.findViewById(R.id.txtPrefix);
-        mChkAdd = (RadioButton) v.findViewById(R.id.chkAdd);
+        mChkAdd = (CheckBox) v.findViewById(R.id.chkAdd);
         mLayoutCompeny = (LinearLayout) v.findViewById(R.id.layoutCompeny);
 
         // Current Date
@@ -53,6 +52,16 @@ public class ProfileFragment extends Fragment {
         Log.d("runtime",formattedDate);
         mDatesave.setText(formattedDate);
         mDatecome.setText(formattedDate);
+        mChkAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mChkAdd.isChecked()){
+                    mLayoutCompeny.setVisibility(View.INVISIBLE);
+                }else{
+                    mLayoutCompeny.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
         return v;
     }
